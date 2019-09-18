e17598315ff2eee8bd58b797300da17463a9e0b3
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-09-18 05:31:20 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,7 @@
-INSERT INTO projects VALUES ('car', 'transportation', 25000, '09/14', '11/22')
+INSERT INTO projects VALUES ('car', 'transportation', 25000, '09/14', '11/22');
+
+
+INSERT INTO users VALUES ('Joe', 22);
+
+
+INSERT INTO pledges VALUES (300, 1, 1)
